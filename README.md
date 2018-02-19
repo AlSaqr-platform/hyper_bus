caf2c833af2890e855da8d0c5ce0d82ba44a4fd1
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2018-02-19 20:44:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Revise HyperBus Implementation
+
+## Getting Started
+
+TBD
+
+## Contribution Style
+
+Please consider the following [style guidelines](https://github.com/pulp-platform/style-guidelines).
---
+++ src/hyperbus.sv
@@ -0,0 +1,20 @@
+// Copyright (C) 2017 ETH Zurich, University of Bologna
+// All rights reserved.
+//
+// This code is under development and not yet released to the public.
+// Until it is released, the code is under the copyright of ETH Zurich and
+// the University of Bologna, and may contain confidential and/or unpublished
+// work. Any reuse/redistribution is strictly forbidden without written
+// permission from ETH Zurich.
+
+// Author:
+// Date:
+// Description:
+
+module hyperbus (
+    input logic clk_i,    // Clock
+    input logic rst_ni,   // Asynchronous reset active low
+
+);
+
+endmodule
