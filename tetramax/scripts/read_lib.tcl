082d56f99a5b65c68a8e20f95b621a33896f86ef
Armin Berger <bergerar@student.ethz.ch>
2018-03-15 13:39:41 +0100

    Add .gitignore and some setup files

---
+++ tetramax/scripts/read_lib.tcl
@@ -0,0 +1,5 @@
+
+read_netlist /usr/pack/umc-65-kgf/umc/ll/uk65lscllmvbbl/b03/tetramax.dz/uk65lscllmvbbl_sdf30.v
+read_netlist /usr/pack/umc-65-kgf/umc/ll/uk65lscllmvbbl/b03/tetramax.dz/stubs.v
+read_netlist /usr/pack/umc-65-kgf/umc/ll/u065gioll25mvir/b04/tetramax.dz/u065gioll25mvir.v
+
