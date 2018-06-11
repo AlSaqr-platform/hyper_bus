4b151d4edc7c57401da1395f4d15d1f9c70a863b
Armin Berger <bergerar@student.ethz.ch>
2018-06-11 14:11:45 +0200

    Add tetramax script

---
+++ tetramax/scripts/hyperbus_macro.tcl
@@ -0,0 +1,12 @@
+source scripts/read_lib.tcl 
+read_netlist ../synopsys/netlists/hyperbus_macro.v 
+run_build_model hyperbus_macro_inflate 
+add_clocks 0 clk_sys_i -shift 
+add_clocks 1 rst_ni 
+add_scan_chains chain1 scan_in_ti scan_out_to 
+add_scan_enables 1 scan_en_ti 
+add_pi_constraints 1 test_en_ti 
+run_drc 
+add_faults -all 
+report_summaries 
+run_atpg -auto_compression 
