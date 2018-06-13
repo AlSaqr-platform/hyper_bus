9db2ee4c459a46af86fae8fa4fc74b7acfa225eb
Armin Berger <bergerar@student.ethz.ch>
2018-06-13 13:49:10 +0200

    Test scan chain

--- tetramax/scripts/hyperbus_macro.tcl
+++ tetramax/scripts/hyperbus_macro.tcl
@@ -1,5 +1,10 @@
 source scripts/read_lib.tcl 
-read_netlist ../synopsys/netlists/hyperbus_macro.v 
+
+
+#read_netlist ../synopsys/netlists/hyperbus_macro.v 
+read_netlist ../encounter/out/hyperbus_macro.v 
+
+
 run_build_model hyperbus_macro_inflate 
 add_clocks 0 clk_sys_i -shift 
 add_clocks 1 rst_ni 
@@ -10,3 +15,17 @@ run_drc
 add_faults -all 
 report_summaries 
 run_atpg -auto_compression 
+
+
+report_faults -summary
+report_faults -level 3 32 -verbose
+
+write_faults reports/faults-au.rpt -replace -class AU 
+write_faults reports/faults-nd.rpt -replace -class ND 
+write_faults reports/faults-ud.rpt -replace -class UD 
+
+write_patterns pattern/pattern.wglflat -replace -internal -format wgl_flat -sorted -order_pins
+
+write_testbench -input pattern/pattern.wglflat_stiltmp -output scanchain_tb -replace
+
+
